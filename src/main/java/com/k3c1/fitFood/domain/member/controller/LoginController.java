e0f0acc955084a322010d3df81fc015224975817
KKT <kimgt2828@gmail.com>
2024-11-03 04:33:16 +0900

    feat: 소셜 로그인 기능 시도

---
+++ src/main/java/com/k3c1/fitFood/domain/member/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.k3c1.fitFood.domain.member.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+    public String loginPate() {
+        return "login";
+    }
+
+}
--- src/main/java/com/k3c1/fitFood/domain/member/entity/Member.java
+++ src/main/java/com/k3c1/fitFood/domain/member/entity/Member.java
@@ -2,9 +2,11 @@ package com.k3c1.fitFood.domain.member.entity;
 
 import jakarta.persistence.*;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@Getter
 
 @Entity
 public class Member extends com.wondrous.board.domain.BaseTimeEntity {
@@ -24,6 +26,9 @@ public class Member extends com.wondrous.board.domain.BaseTimeEntity {
     @Column
     private String picture;
 
+    @Column
+    private String role;
+
     @Builder
     public Member(String name, String email, String picture) {
         this.name = name;
@@ -34,6 +39,20 @@ public class Member extends com.wondrous.board.domain.BaseTimeEntity {
     public Member update(String picture) {
         this.picture = picture;
         return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void update(String name, String email, String role) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
     }
 
     //provider
