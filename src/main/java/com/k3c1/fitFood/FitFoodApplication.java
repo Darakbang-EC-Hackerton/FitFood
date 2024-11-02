b2cd09a13feecb12a7f53a3ba4df79c59d9b5e93
KKT <kimgt2828@gmail.com>
2024-11-02 14:51:56 +0900

    setting

---
+++ src/main/java/com/k3c1/fitFood/FitFoodApplication.java
@@ -0,0 +1,13 @@
+package com.k3c1.fitFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k3c1/fitFood/FitFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k3c1.fitFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
